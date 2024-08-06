e58778bee9fca8f5a30bdeb482a2cb3ca4e40439
Daniel Li <danielgongli.65@gmail.com>
2024-08-06 17:59:44 -0400

    changed compression codecs again

--- src/Visualizer.cpp
+++ src/Visualizer.cpp
@@ -46,7 +46,7 @@ void Visualizer::render(int screenWidth, int screenHeight, int renderFPS, std::s
     hsize_t dataIndexRow = 0;
     hsize_t dataIndexCol = 0;
 
-    int codec = cv::VideoWriter::fourcc('x', '2', '6', '4');
+    int codec = cv::VideoWriter::fourcc('a', 'v', 'c', '1');
     cv::VideoWriter videoWriter;
 
     raylib::Window window(screenWidth, screenHeight, TextFormat("SatPathVisualizer %ix%i %s", screenWidth, screenHeight, outputFile.c_str()));
